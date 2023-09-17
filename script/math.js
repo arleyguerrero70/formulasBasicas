3974e3b7913b91de1cc0567bff9c0489a6674bd3
Arley <aguerrero0615@gmail.com>
2023-09-16 20:49:43 -0500

    Feat: Se crea la función para calcular el area, diametro y perímetro del círculo

--- script/math.js
+++ script/math.js
@@ -1,29 +1,30 @@
+console.group("Cuadrado")
 function calcularCuadrado (lado){
     return{
         perimetro: lado * 4,
         area: lado * lado
     }
 }
+console.groupEnd("Cuadrado")
 
-const trianguloArea1 = 6
-const trianguloArea2 = 6
-const trianguloBase = 4
-const trianguloAltura = 10
-
-const trianguloPerimetro = trianguloArea1 + trianguloArea2 + trianguloBase
-const trianguloArea = (trianguloBase * trianguloAltura) / 2
-
-console.log({
-    trianguloArea1,
-    trianguloArea2,
-    trianguloBase,
-    trianguloPerimetro,
-    trianguloArea
-})
-
+console.group("Triángulo")
 function calcularTriangulo (lado1, lado2, base, altura){
     return{
         perimetro: lado1 + lado2 + base,
         area: (base * altura) / 2
     }
 }
+console.groupEnd("Triángulo")
+
+console.group("Círculo")
+
+function calcularCirculo (radio){
+    const diametro = radio * 2
+    const radioAlCuadrado = Math.pow (radio, 2)
+    return{
+        circunferencia: diametro * Math.PI.toFixed(2),
+        area: radioAlCuadrado * Math.PI.toFixed(2)
+    }
+}
+
+console.groupEnd("Círculo")
